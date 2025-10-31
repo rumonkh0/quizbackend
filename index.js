a872659d5bcdc57e64f0a9a08fc7b42c4808a49a
rumonkh0 <rummankh0@gmail.com>
2025-10-31 15:28:28 +0600

    cors up

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://quizfrontend-six.vercel.app",
     credentials: true,
     maxAge: 14400,
   })
